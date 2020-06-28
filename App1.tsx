e86aa44f502eea4d3971f4b8d5287eea5c5e3300
imbora <bora2116@naver.com>
2020-06-28 20:06:24 +0900

    toggle condition

--- App1.tsx
+++ App1.tsx
@@ -3,7 +3,6 @@ import { useAnimation } from "framer";
 import { useAnimatedState } from "framer";
 
 // 만약에 origin 을 도중에 바꾼다면
-
 var videoControl;
 var data, setData;
 export function Video(): Override {
@@ -32,5 +31,42 @@ export function Profile(): Override {
     },
 
     animate: profileControl,
+  };
+}
+
+// if + props + toggle
+var handleControl;
+export function Handle(): Override {
+  handleControl = useAnimation();
+  return {
+    animate: handleControl,
+  };
+}
+
+var isToggleOn = 0;
+var bgControl;
+export function Bg(props): Override {
+  bgControl = useAnimation();
+  return {
+    animate: bgControl,
+    onTap: () => {
+      if (isToggleOn === 0) {
+        isToggleOn = 1;
+        handleControl.start({
+          left: 55,
+        });
+        bgControl.start({
+          background: "red",
+        });
+      } else if (isToggleOn === 1) {
+        isToggleOn = 0;
+        handleControl.start({
+          left: 5,
+        });
+        bgControl.start({
+          background: props.background,
+        });
+      }
+    },
   };
 }
